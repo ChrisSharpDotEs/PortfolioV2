9bd2c4856cdded0fde427b017d36644dc45fa23e
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2024-10-02 21:01:20 +0100

    Update

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
     loop: true,  // Habilitar el bucle de imágenes
-    slidesPerView: 4,  // Mostrar 3 imágenes a la vez
+    slidesPerView: 5,  // Mostrar 3 imágenes a la vez
     spaceBetween: 30,  // Separación entre imágenes (en píxeles)
     slidesPerGroup: 1,  // Deslizar una imagen por vez
     pagination: {
